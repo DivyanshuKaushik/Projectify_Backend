153439274ee064211ddb80b47c21aa098fd1d602
Rishit Shivesh <rishitshivesh@gmail.com>
2023-01-03 23:23:01 +0530

    data

--- src/models/project.model.js
+++ src/models/project.model.js
@@ -3,22 +3,22 @@ import sequelize from "../db";
 import Student from "./student.model";
 
 const Project = sequelize.define("Project", {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    comments: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  comments: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
 });
 Project.hasMany(Student, {
-    foreignKey: {
-        name: "project_id",
-        allowNull: true,
-        // onDelete: "CASCADE",
-        // onUpdate: "CASCADE",
-    },
+  foreignKey: {
+    name: "project_id",
+    allowNull: true,
+    // onDelete: "CASCADE",
+    // onUpdate: "CASCADE",
+  },
 });
 
 export default Project;
